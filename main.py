5be79f8b3cfd15859da449d3549472eefb8ecaf1
darkharasho <project96+github@gmail.com>
2023-10-29 20:29:48 -0700

    50 50

--- main.py
+++ main.py
@@ -95,12 +95,12 @@ async def on_message(message):
         if settings.OPEN_AI_KEY:
             if Config.bot_chat_channel_ids():
                 if message.channel.id in Config.bot_chat_channel_ids():
-                    if "fleas" in message.author.name:
+                    if "fleas" in message.author.name and random.randint(0, 10) % 2 == 0:
                         await message.channel.send("Haro sends his regards. No more draining my credits, flis.")
                     else:
                         await conversation_client.chunked_converse(message.author, message)
             else:
-                if "fleas" in message.author.name:
+                if "fleas" in message.author.name and random.randint(0, 10) % 2 == 0:
                     await message.channel.send("Haro sends his regards. No more draining my credits, flis.")
                 else:
                     await conversation_client.chunked_converse(message.author, message)
